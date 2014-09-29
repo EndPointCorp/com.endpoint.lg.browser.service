779622396f4722edf797e57ae9f4700859ec05a3
Joshua Tolley <josh@endpoint.com>
2014-09-29 15:07:04 -0600

    Add annotation so JSON from chromium debug can enlarge without causing exceptions

--- src/main/java/com/endpoint/lg/browser/service/BrowserDebugInfo.java
+++ src/main/java/com/endpoint/lg/browser/service/BrowserDebugInfo.java
@@ -1,5 +1,8 @@
 package com.endpoint.lg.browser.service;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BrowserDebugInfo {
     public BrowserTabInfo[] tabs;
 }
--- src/main/java/com/endpoint/lg/browser/service/BrowserTabInfo.java
+++ src/main/java/com/endpoint/lg/browser/service/BrowserTabInfo.java
@@ -1,5 +1,8 @@
 package com.endpoint.lg.browser.service;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BrowserTabInfo {
     public String description, devtoolsFrontendUrl, faviconUrl, id, title, type, url, webSocketDebuggerUrl;
 }
